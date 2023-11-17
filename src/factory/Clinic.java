a5cb89303e82270c0cb821cd5a2e693e992b2b27
Marc Agustin Javier <147635825+Nitsucruin@users.noreply.github.com>
2023-11-17 11:00:28 +0800

    Update Clinic.java

--- src/factory/Clinic.java
+++ src/factory/Clinic.java
@@ -5,44 +5,62 @@ import java.util.Scanner;
 public class Clinic {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Choose a pet");
-        System.out.println("1. Dog");
-        System.out.println("2. Cat");
-        System.out.println("3. Exit");
-        System.out.print("Enter number: ");
-        Integer choice = input.nextInt();
-
+        
         PetRecord petRecord = new PetRecord();
-        Pet pet;
+        Pet pet = null;
+        int choice;
 
-        switch (choice) {
-            case 1:
-                pet = new Dog();
-                petRecord.setPetName("Bantay");
-                petRecord.setPetId("P001");
-                petRecord.setPet(pet);
-                ((Dog) pet).setBreed("German shepherd");
-                break;
+        do{
+            System.out.println("Choose a pet");
+            System.out.println("1. Dog");
+            System.out.println("2. Cat");
+            System.out.println("3. Exit");
+            System.out.print("Enter number: ");
+            choice = input.nextInt();
+            input.nextLine();
 
-            case 2:
-                pet = new Cat();
-                petRecord.setPetName("Muning");
-                petRecord.setPetId("P002");
-                petRecord.setPet(pet);
-                ((Cat) pet).setNoOfLives(9);
-                break;
-            case 3:
-                System.exit(0);
+            if(choice == 1 || choice <= 3){
+                switch (choice) {
+                case 1:
+                    pet = new Dog();
+                    System.out.print("Enter dog's name: ");
+                    String dogName = input.nextLine();
+                    petRecord.setPetName(dogName);
+                    String PetIdDog = input.nextLine();
+                    petRecord.setPetId(PetIdDog);    
+                    petRecord.setPet(pet);
+                    System.out.print("Enter Dog's breed: ");
+                    String dogBreed = input.nextLine();
+                    ((Dog) pet).setBreed(dogBreed);
+                    break;
 
-        }
+                case 2:
+                    pet = new Cat();
+                    System.out.print("Enter cat's name: ");
+                    String catName = input.nextLine();
+                    petRecord.setPetName(catName);
+                    System.out.print("Enter Cat's ID: ");
+                    String PetIdCat = input.nextLine();
+                    petRecord.setPetId(PetIdCat);
+                    petRecord.setPet(pet);
+                    ((Cat) pet).setNoOfLives(9);
+                    break;
+                case 3:
+                    System.exit(0);
 
-        System.out.println("Pet id is " + petRecord.getPetId());
-        System.out.println("Pet name is " + petRecord.getPetName());
-        System.out.println("Pet kind: " + petRecord.getPet().getClass().getSimpleName());
-        System.out.println("Communication sound: " + petRecord.getPet().makeSound());
-        System.out.println("Play mode: " + petRecord.getPet().play());
-
+            }
+                System.out.println("Pet id is " + petRecord.getPetId());
+                System.out.println("Pet name is " + petRecord.getPetName());
+                System.out.println("Pet kind: " + petRecord.getPet().getClass().getSimpleName());
+                System.out.println("Communication sound: " + petRecord.getPet().makeSound());
+                System.out.println("Play mode: " + petRecord.getPet().play());
+                System.out.println();
+            }else{
+                System.out.println("Invalid number. Select 1-3 only.");
+            	System.out.println();
+            }
+            
+        } while (choice != 3);
 
     }
 
